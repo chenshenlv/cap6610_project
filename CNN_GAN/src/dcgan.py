b02fc7b7fe462d5632dae91689640906af264ebf
Jason Lv <chenshen.lv@aliyun.com>
2020-12-11 03:10:05 -0500

    add

---
+++ CNN_GAN/src/dcgan.py
@@ -0,0 +1,68 @@
+import torch.nn as nn
+# generator
+class Generator(nn.Module):
+    def __init__(self, nz):
+        super(Generator, self).__init__()
+        self.nz = nz
+        self.main = nn.Sequential(
+            # nz will be the input to the first convolution
+            nn.ConvTranspose2d(
+                nz, 1024, kernel_size=4,
+                stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                1024, 512, kernel_size=4,
+                stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                512, 256, kernel_size=4,
+                stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                256, 128, kernel_size=4,
+                stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(
+                128, 1, kernel_size=4,
+                stride=2, padding=1, bias=False),
+            nn.Tanh()
+        )
+    def forward(self, input):
+        return self.main(input)
+
+# discriminator
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+            nn.Conv2d(
+                1, 128, kernel_size=4,
+                stride=2, padding=1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(
+                128, 256, kernel_size=4,
+                stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(
+                256, 512, kernel_size=4,
+                stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(
+                512, 1024, kernel_size=4,
+                stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(
+                1024, 1, kernel_size=4,
+                stride=1, padding=0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
